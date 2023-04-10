b0725e19a65ba90e1349a77cb380d2314123dff3
Madagambada <47931256+Madagambada@users.noreply.github.com>
2023-04-10 10:38:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo -n "Set toolchain vars... "
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++ 
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/lib
-export LD_RUN_PATH=$LD_RUN_PATH:/usr/lib
-export C_INCLUDE_PATH=C_INCLUDE_PATH$:/usr/include
-export CPLUS_INCLUDE_PATH=CPLUS_INCLUDE_PATH$:/usr/include
+export AR=/usr/bin/llvm-ar-16
+export AS=/usr/bin/llvm-as-16
+export CC=/usr/bin/clang-16
+export CXX=/usr/bin/clang++-16
+export LD=/usr/bin/llvm-ld-16
+export RANLIB=/usr/bin/llvm-ranlib-16
+export STRIP=/usr/bin/llvm-strip-16
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
+export LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/include
 
 echo -n "Create build directory... "
 mkdir build
