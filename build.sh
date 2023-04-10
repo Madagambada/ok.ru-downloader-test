e913becf4e7690e8fd763777102098e1b131fa5c
Madagambada <47931256+Madagambada@users.noreply.github.com>
2023-04-10 07:28:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo -n "Set toolchain vars... "
 export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++ clang++
+export CXX=/usr/bin/clang++
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
 export LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
 export C_INCLUDE_PATH=C_INCLUDE_PATH$:/usr/local/include
